6a7516533ed4d91f332b9c4ead58af03226c5395
lengochuyen25 <huyenln.hvnh@gmail.com>
2019-09-06 10:29:08 +0700

    a

---
+++ src/main/java/SystemTime.java
@@ -0,0 +1,11 @@
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
+
